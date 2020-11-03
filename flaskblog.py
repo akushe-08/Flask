b7becb594b862f55ab685a47a0a861b852d81765
akushe-08 <kalane.aks@gmail.com>
2020-11-04 00:02:48 +0530

    Commit 3.0

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+import email_validator
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f4fac2f039c269ccfc46532ef1502c81'
+
 
 posts = [
     {
@@ -25,8 +30,29 @@ def home():
 
 
 @app.route('/about')
-def about_page():
+def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check Username and Password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
